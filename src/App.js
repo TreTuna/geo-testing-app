76aa087498ba70b67b34bfe3c131ebefb0c2b5e6
Tré Ammatuna <tretuna@amazon.com>
2021-11-18 16:29:40 -0800

    chore: initial geo testing app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from "aws-amplify";
+import { Map } from "./Map";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Map />
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </main>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
